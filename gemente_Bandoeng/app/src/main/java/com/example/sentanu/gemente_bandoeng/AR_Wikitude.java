357de7fbf6c447b1697e7488c6651ca1b5b35076
Sentanu <sentanu00@gmail.com>
2016-06-22 14:04:07 +0700

    awal yang baik..:D

---
+++ gemente_Bandoeng/app/src/main/java/com/example/sentanu/gemente_bandoeng/AR_Wikitude.java
@@ -0,0 +1,13 @@
+package com.example.sentanu.gemente_bandoeng;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AR_Wikitude extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ar__wikitude);
+    }
+}
